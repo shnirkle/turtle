b1168108e7c2cea0b9f91a9cda6579758c6cf8e2
shnirkle <leventd444@gmail.com>
2024-08-17 14:27:22 +0200

    test 2

--- main.lua
+++ main.lua
@@ -1,1 +1,25 @@
 print("hello World")
+fuel_slot = 16
+torch_slot = 15
+fuel_id = 263
+target_fuel = 1000
+fuel_level = 0
+
+dx,dy,dz = 0
+
+function fuel_once()
+    turtle.select(fuel_id)
+    local passed, error = turtle.refuel()
+    if passed then
+        fuel_level = turtle.getFuelLevel()
+        print("Refuel_Level: %d":format(fuel_level))
+    else
+        print("Kein Fuel in Slot 16")
+    end
+end
+function fuel_up()
+    while fuel_level < target_fuel do
+        fuel_once()
+    end
+
+function quarry()
