2765bf749bc38a00a524bc04c7cd30e868ae2aa0
shnirkle <leventd444@gmail.com>
2024-08-17 18:46:23 +0200

    quick quarry test #2

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ fuel_id = 263
 target_fuel = 1000
 fuel_level = 0
 
+curr_Dir = "f"
 dx,dy,dz = 0
 
 
@@ -14,13 +15,15 @@ function fuel_once()
     if passed then
         fuel_level = turtle.getFuelLevel()
         print(("Refuel_Level: %d"):format(fuel_level))
+        return true
     else
         print("Kein Fuel in Slot 16")
+        return false
     end
 end
 function fuel_up()
     while fuel_level < target_fuel do
-        fuel_once()
+        assert(fuel_once())
     end
 end
 
@@ -28,5 +31,51 @@ function quarry()
     fuel_up()
 end
 
-quarry()
+function return()
+    move_to(0,0,0)
+end
+function move_to(x,y,z) 
+    if dx > 0 then
+        move_b(dx)
+    else 
+        move_f(dx * -1)
+    end
+    if dy > 0 then
+        move_l(dy)
+    else 
+        move_r(dy * -1)
+    end
 
+function move(dir, amount)
+    if "f" then
+        set_Dir("f")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    elseif "b" then
+        set_Dir("b")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    elseif "l" then
+        set_Dir("b")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    elseif "r" then
+        set_Dir("b")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    elseif "u" then
+        set_Dir("b")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    elseif "d" then
+        set_Dir("b")
+        for i = 0, amount, 1 do
+            turtle.forward
+        end
+    end
+end
---
+++ test.lua
@@ -0,0 +1,4 @@
+dx = -17
+for i = dx, (dx>0) and -1 or 1, 0 do
+    print(i)
+end
