eedbba1837d4d52061ac772117eb4a5ee6dabcae
shnirkle <leventd444@gmail.com>
2024-08-17 18:46:09 +0200

    quick quarry test #1

---
+++ quick_quarry.lua
@@ -0,0 +1,39 @@
+
+fuel_slot = 16
+torch_slot = 15
+fuel_id = 263
+target_fuel = 1000
+fuel_level = 0
+
+function fuel_once()
+    turtle.select(fuel_slot)
+    local passed, error = turtle.refuel()
+    if passed then
+        fuel_level = turtle.getFuelLevel()
+        print(("Refuel_Level: %d"):format(fuel_level))
+        return true
+    else
+        print("Kein Fuel in Slot 16")
+        return false
+    end
+end
+function fuel_up()
+    while fuel_level < target_fuel do
+        assert(fuel_once())
+    end
+end
+function deposit()
+    for i = 1, 16, 1 do
+        if turtle.drop() == false then
+            turtle.up()
+            z += 1
+        end
+    end
+    for i = 0, z, 1 do
+        turtle.down()
+    end
+    turtle.right()
+    turtle.right()
+end
+function quarry
+    
